f94002aab98d5ea970cb3f6475675e9dae7349e4
Arnaud Doucerain <adoucerain@octo.com>
2017-05-22 23:30:46 +0200

    Improve QP migration

--- db/migrate/20170522203332_remove_quartier_prioritaires.rb
+++ db/migrate/20170522203332_remove_quartier_prioritaires.rb
@@ -1,5 +1,16 @@
 class RemoveQuartierPrioritaires < ActiveRecord::Migration[5.0]
-  def change
+  def up
     drop_table :quartier_prioritaires
+  end
+
+  def down
+    create_table :quartier_prioritaires do |t|
+      t.string :code
+      t.string :nom
+      t.string :commune
+      t.text :geometry
+    end
+
+    add_reference :quartier_prioritaires, :dossier, references: :dossiers
   end
 end
