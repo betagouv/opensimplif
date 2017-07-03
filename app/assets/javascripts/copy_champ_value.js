dc64726c572266e0abf8423f5354d692c6484762
Arnaud Doucerain <adoucerain@octo.com>
2017-07-03 18:31:29 +0200

    Champ comment modal design improvement

--- app/assets/javascripts/copy_champ_value.js
+++ app/assets/javascripts/copy_champ_value.js
@@ -1,5 +1,5 @@
 function copyChampValueToCommentField(){
     $("#copy_champ_value button").on('click', function(){
-        $('#modalCommentairesDossierParChamp .wysihtml5-sandbox').contents().find('body').html($.trim($("#champ #value").html()));
+        $('#modalCommentairesDossierParChamp .wysihtml5-sandbox').contents().find('body').html($.trim($("#commentaires_flux #value").html()));
     });
 }
